1c2e1a94c55fcf2b840b8c64533aac57c8864fb6
Patheit <andrewtnguyen0106@gmail.com>
2025-05-27 20:35:53 -0700

    Finished the Queues, need to work on fetchUrls

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"net/http"
+)
+
+/*
+	"io"
+	"strings"
+	"time"
+	"context"
+	"log"
+	"golang.org/x/net/html"
+*/
+
+type Queue struct {
+	elementsInQueue int
+	totalQueued     int
+	mu              sync.Mutex
+	elements        []string
+}
+
+func (q *Queue) enQueue(url string) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	q.elements = append(q.elements, url)
+	q.totalQueued++
+	q.elementsInQueue++
+}
+
+func (q *Queue) deQueue() string {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	val := "Queue is empty"
+	if len(q.elements) > 0 {
+		val = q.elements[0]
+		q.elements = q.elements[1:]
+		q.elementsInQueue--
+	}
+
+	return val
+}
+
+func (q *Queue) fetchUrls(url string) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	res, err := http.Get(url)
+
+	//meaning that there's an error and its not blank
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println(res)
+}
+
+func main() {
+	queue := Queue{}
+	seed := "https://www.wikipedia.org/"
+
+	queue.enQueue((seed))
+	
+	queue.fetchUrls(queue.deQueue())
+
+}
